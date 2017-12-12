6c8c3083de8b6fac9a3afe17ad96f212def6e3d4
billyzou <yz3352@nyu.edu>
2017-12-13 00:54:04 +0800

    change typo

---
+++ Final_Project/js/script.js
@@ -0,0 +1,23 @@
+
+function demoPlay(){
+	
+	document.getElementById("words").style.display = "none";
+	//document.getElementById("clips").style.animation = "clips 2s 1 forwards";
+	document.getElementById("video1").style.display = "block";
+	document.getElementById("video2").style.display = "block";
+	document.getElementById("video3").style.display = "block";
+	document.getElementById("backbutton3").style.display = "block";
+	document.getElementById("Back").style.display = "block";
+
+}
+
+function Back(){
+	document.getElementById("words").style.display = "block";
+	//document.getElementById("clips").style.animation = "clips 2s 1 forwards";
+	document.getElementById("video1").style.display = "none";
+	document.getElementById("video2").style.display = "none";
+	document.getElementById("video3").style.display = "none";
+	document.getElementById("backbutton3").style.display = "none";
+	document.getElementById("Back").style.display = "none";
+
+}
